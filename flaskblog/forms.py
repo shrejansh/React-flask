b00f976a08b7f05680cf8fcb6522c62333d0dcb2
danbo <shreyanshanand54@gmail.com>
2021-05-18 17:43:30 +0530

    Added ability to update user info and default profile pic

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from email_validator import validate_email, EmailNotValidError
 from flaskblog.models import User
+from flask_login import current_user
 
 class  RegistrationForm(FlaskForm):
     username=StringField('username', validators=[DataRequired(), Length(min=2,max=20)])
@@ -27,3 +29,21 @@ class  LoginForm(FlaskForm):
     password=PasswordField('Password',validators=[DataRequired()])
     remember= BooleanField('Remember Me')
     submit=SubmitField('Login')
+
+class  UpdateAccountForm(FlaskForm):
+    username=StringField('username', validators=[DataRequired(), Length(min=2,max=20)])
+    email=StringField('email', validators=[DataRequired()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
+    submit=SubmitField('Update')
+
+    def validate_username(self , username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first()
+            if user:
+                raise ValidationError('Username taken, please use another username')
+
+    def validate_email(self , email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email = email.data).first()
+            if user:
+                raise ValidationError('emial taken, please use another emial')
